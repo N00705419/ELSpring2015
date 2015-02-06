8f551e63ece3ae53e4eb91e4cf88d90331a4b9f1
Franz Vezuli <vezuli46@hawkmail.newpaltz.edu>
2015-02-06 00:14:13 -0500

    Changed script to write to CSV
    Outputting to a .csv file.
    
    Data is held in data.csv, but haven't figured out how to append only to
    the last line. Have to read up on opening csv files in python.

--- code/rpi-temp-script.py
+++ code/rpi-temp-script.py
@@ -12,16 +12,25 @@ import csv
 """ Log Current Time, Temperature in Celsius and Fahrenheit
  Returns a list [time, tempC, tempF] """
  
+ 
 def readTemp():
 	tempfile = open("/sys/bus/w1/devices/28-0000069816ee/w1_slave")
 	tempfile_text = tempfile.read()
-	currentTime=time.strftime('%x %X %Z')
+	currentDate=time.strftime('%x')
+	currentTime=time.strftime('%x')
 	tempfile.close()
 	tempC=float(tempfile_text.split("\n")[1].split("t=")[1])/1000
 	tempF=tempC*9.0/5.0+32.0
-	with open('data.csv', 'wb') as f:
-		writer = csv.writer(f)
-		writer.writerows(currentTime, tempC, tempF)
-	return [currentTime, tempC, tempF]
 	
-print readTemp() 
+	"""Write out to a CSV file, but first make sure it writes only to the last line"""
+	
+	with open('data.csv', 'wb') as f:
+		f.seek(0, os.SEEK_END)
+		writer = csv.writer(f, delimiter=',', lineterminator='\n')
+		data = [currentDate, currentTime, tempC, tempF]
+		writer.writerow(data)
+		
+	return [currentDate, currentTime, tempC, tempF]
+
+	
+print readTemp()
