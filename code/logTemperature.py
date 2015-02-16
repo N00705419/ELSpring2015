2c3301782100af703280221de15ddc96b7c67a0c
Franz Vezuli <vezuli46@hawkmail.newpaltz.edu>
2015-02-16 09:21:25 -0500

    Updated temperature script
    - Moved some files.
    - Created Database.
    - Changed sensor script.

--- code/logTemperature.py
+++ code/logTemperature.py
@@ -5,9 +5,11 @@
 
 import os
 import time
-
-"""Need this to write to CSV File"""
-import csv 
+import sqlite3 as mydb
+import sys
+ 
+"""Need this to write to CSV File
+   import csv """
 
 """ Log Current Time, Temperature in Celsius and Fahrenheit
  Returns a list [time, tempC, tempF] """
@@ -16,21 +18,23 @@ import csv
 def readTemp():
 	tempfile = open("/sys/bus/w1/devices/28-0000069816ee/w1_slave")
 	tempfile_text = tempfile.read()
-	currentDate=time.strftime('%x')
-	currentTime=time.strftime('%x')
+	currentTime=time.strftime('%x %X %Z')
 	tempfile.close()
 	tempC=float(tempfile_text.split("\n")[1].split("t=")[1])/1000
 	tempF=tempC*9.0/5.0+32.0
-	
-	"""Write out to a CSV file, but first make sure it writes only to the last line"""
+		
+	"""Write out to a CSV file, but first make sure it writes only to the last line
 	
 	with open('data.csv', 'wb') as f:
 		f.seek(0, os.SEEK_END)
 		writer = csv.writer(f, delimiter=',', lineterminator='\n')
 		data = [currentDate, currentTime, tempC, tempF]
-		writer.writerow(data)
+		writer.writerow(data)"""
 		
-	return [currentDate, currentTime, tempC, tempF]
-
+	con = mydb.connect('temperature.db')
+	cur = con.cursor()
+	cur.execute("insert into tempData values(currentTime, tempC, tempF)");
+		
+	return "Current temperature is: " + str(tempF) + "\nTemperature logged"
 	
 print readTemp()
